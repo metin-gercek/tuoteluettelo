33aadcc4c6d306b592819e1bf7fda10b28b1b45e
metin-gercek <metin.gercek@edu.bc.fi>
2022-03-17 12:55:25 +0200

    config for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/tuoteluettelo'));
+app.use(express.static('./dist/tuoteluettelo'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/tuoteluettelo/'}),
