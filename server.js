f9b794f9ca893c491a366fbfc7530d37aba6fa46
metin-gercek <metin.gercek@edu.bc.fi>
2022-03-17 12:49:19 +0200

    config for heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tuoteluettelo'));
+app.use(express.static(__dirname + './dist/tuoteluettelo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tuoteluettelo/'}),
+    res.sendFile('./dist/tuoteluettelo/index.html', {root: 'dist/tuoteluettelo/'}),
 );
 
 // Start the app by listening on the default Heroku port
